fca4c78e36551fa4ba5a01d833601d5c23aa3008
ChowSinChong <chowsin.chong96@gmail.com>
2022-11-04 17:09:03 +0800

    Wk1-HW1, IC2

--- ChongChowSin/Week1/Homework/Class1/custom.js
+++ ChongChowSin/Week1/Homework/Class1/custom.js
@@ -1,72 +1,193 @@
+function logOrAlert(value){
+    // alert(value);
+    console.log(value);
+}
+
+/* 00_primitive_data_types */
 function AgeCalc(){
     let currentYear = new Date().getFullYear();
     let birthYear = prompt("Birth Year:");
     if(birthYear > currentYear){
-        alert("You have not born yet.");
+        logOrAlert("You have not born yet.");
         return;
     }
     let age = currentYear - birthYear;
-    alert(`They are either ${age} or ${age-1}`);
+    logOrAlert(`They are either ${age} or ${age-1}`);
 }
 
 function LifeTimeCalc(){
     let age = parseInt(prompt("Your age:"));
     let maxAge = parseInt(prompt("Your max age:"));
     if(age > maxAge){
-        alert("Age cannot greater than max age.");
+        logOrAlert("Age cannot greater than max age.");
         return;
     }
     let numOfDays = (maxAge - age) * 365;
-    alert(`You will need ${numOfDays} to last you until the ripe old age of ${maxAge}`);
+    logOrAlert(`You will need ${numOfDays} to last you until the ripe old age of ${maxAge}`);
 }
 
 function Geometrizer(){
     let r = parseFloat(prompt("Enter radius of the circle:"));
-    alert(`The circumference is ${2 * r * Math.PI} \nThe area is ${Math.PI*(r**2)}`);
+    logOrAlert(`The circumference is ${2 * r * Math.PI} \nThe area is ${Math.PI*(r**2)}`);
 }
 
 function TemperatureConverter(){
     let temp = parseFloat(prompt("Enter temperature: "));
     let cf = prompt("Celsius or Fahrenheit? (C/F)");
     if(cf.toUpperCase() == "C")
-        alert(`${temp}°C is ${(temp * 9/5) + 32}°F`);
+        logOrAlert(`${temp}°C is ${(temp * 9/5) + 32}°F`);
     else
-        alert(`${temp}°F is ${(temp - 32) * 5/9}°C.`);
+        logOrAlert(`${temp}°F is ${(temp - 32) * 5/9}°C.`);
 }
+
+/* 01_conditionals */
 
 function WorldTranslator(){
     let lang = prompt("English/French/Malay? (En/Fr/My)");
     switch(lang.toUpperCase()){
         case "EN":
-            alert("Hello World");
+            logOrAlert("Hello World");
             break;
         case "FR":
-            alert("Bonjour le monde");
+            logOrAlert("Bonjour le monde");
             break;
         case "MY":
-            alert("Hai dunia");
+            logOrAlert("Hai dunia");
             break;
         default:
-            alert("Invalid Language");
+            logOrAlert("Invalid Language");
     }
 }
 
-function GradeAssigner(){
-    let s = parseFloat(prompt("Your score:"));
+function GradeAssigner(s){
+    // if(!s)
+    //     s = parseFloat(prompt("Your score:"));
+
     if(s >= 80)
-        alert(`You got 'A'`);
+        logOrAlert(`You got 'A'`);
     else if(s >= 70)
-        alert(`You got 'B'`);
+        logOrAlert(`You got 'B'`);
     else if(s >= 50)
-        alert(`You got 'C'`);
+        logOrAlert(`You got 'C'`);
     else if(s >= 40)
-        alert(`You got 'D'`);
+        logOrAlert(`You got 'D'`);
     else if(s >= 20)
-        alert(`You got 'E'`);
+        logOrAlert(`You got 'E'`);
     else
-        alert(`You 'F'ailed`);
+        logOrAlert(`You 'F'ailed`);
 }
 
 function AirCon(){
-    
+    const hotTemp = 38;
+    let temp = parseFloat(prompt("Temperature:"));
+    let functional = (prompt("A/C Function? (T/F)")).toUpperCase() == "T" ? true: false;
+    if(functional && temp >= hotTemp){
+        logOrAlert(`Turn on the A/C Please`);
+        return;
+    }
+    if(!functional){
+        if(temp >= hotTemp)
+            logOrAlert(`Fix the A/C now! It's hot!`);
+        else
+            logOrAlert(`Fix the A/C whenever you have the chance... It's cool...`);
+    }
+}
+
+function VoteGovernment(){
+    let age = prompt("Your age:")
+    if(age >= 35)
+        logOrAlert(`You can vote AND hold any place in government!`);
+    else if(age >= 25)
+        logOrAlert(`You can vote AND run for the Senate!`);
+    else if(age >= 18)
+        logOrAlert(`You can vote!`);
+    else
+        logOrAlert(`You can't vote yet`);
+}
+
+function Golf(par, score){
+    // if(!par)
+    //     par = 18; //not sure
+    // if(!score)
+    //     score = prompt("Score:");
+
+    if(score == 1)
+        logOrAlert(`Hole in one`);
+    else if(score <= par -2)
+        logOrAlert(`Eagle`);
+    else if(score == (par-1))
+        logOrAlert(`Birdie`);
+    else if(score == par)
+        logOrAlert(`Par`);
+    else if(score == (par+1))
+        logOrAlert(`Bogey`);
+    else if(score ==(par+2))
+        logOrAlert(`Double Bogey`);
+    else if(score >= (par+3))
+        logOrAlert(`Not sure`);
+    else 
+        logOrAlert("Invalid input");
+}
+
+//Refer link below
+//https://stackoverflow.com/questions/17572873/how-can-i-check-if-a-string-is-all-uppercase-in-javascript
+String.prototype.isUpperCase = function() {
+    return this.valueOf().toUpperCase() === this.valueOf();
+};
+
+function SergeSays(){
+    let input = prompt("Enter message:");
+    if(input.endsWith('?'))
+        logOrAlert(`Sure.`);
+    else if(input.isUpperCase())
+        logOrAlert(`Woah, chill out!`)
+    else if(!input)
+        logOrAlert(`Fine. Be that way!`)
+    else
+        logOrAlert(`Whatever.`)
+}
+
+//using library from https://github.com/plurals/pluralize
+import Pluralize from 'pluralize';
+
+function Pluralizer(noun,num){
+    // let noun = prompt("Enter noun:")
+    // let num = parseInt(prompt("Enter number:"));
+
+    return Pluralize(noun,num);
+}
+
+//https://www.geeksforgeeks.org/rock-paper-and-scissor-game-using-javascript/
+function Rest(){
+
+}
+
+/* 02_loops */
+function EvenOdd(){
+    for(i = 0; i<=20;i++)
+        i%2==0? logOrAlert(`${i} is even.`) : logOrAlert(`${i} is odd.`);
+}
+
+function MultipleTable(){
+    for(i=1; i<=12;i++)
+        for(j=1;j<=12;j++)
+            logOrAlert(`${j} * ${i} = ${j*i}`);
+}
+
+function GradeAssignerLoop(){
+    for(i=60;i<=100;i++)
+        GradeAssigner(i);
+}
+
+function GolfLoop(){
+    for(i=1;i<=8;i++)
+        Golf(5,i);
+}
+
+function BottlesBeer(){
+    let count = 100;
+    while(count >= 0){
+        btl = Pluralizer("bottle",count);
+        logOrAlert(`${count == 0? "No more" : count} ${btl} of beer on the wall, ${count} ${btl} of beer.\nTake one down and pass it around, ${count-- == 0? "no more" : count} ${btl} of beer on the wall.`);
+    }
 }
--- ChongChowSin/Week1/In-Class/Class2/custom.js
+++ ChongChowSin/Week1/In-Class/Class2/custom.js
@@ -1,19 +1,19 @@
-// // let rainbowColors = [];
-// // rainbowColors.push("orange");
-// // rainbowColors.unshift("red");
-// // rainbowColors.push("yellow");
-// // rainbowColors.push("green","blue","indigo","violet");
-// // console.log(rainbowColors);
-// // console.log(`Length: ${rainbowColors.length}`);
-// // console.log(`Second Item: ${rainbowColors[1]}`);
-// // console.log(`Last Item: ${rainbowColors[rainbowColors.length -1]}`);
-// // console.log(`Blue index: ${rainbowColors.indexOf("blue")}`);
+// let rainbowColors = [];
+// rainbowColors.push("orange");
+// rainbowColors.unshift("red");
+// rainbowColors.push("yellow");
+// rainbowColors.push("green","blue","indigo","violet");
+// console.log(rainbowColors);
+// console.log(`Length: ${rainbowColors.length}`);
+// console.log(`Second Item: ${rainbowColors[1]}`);
+// console.log(`Last Item: ${rainbowColors[rainbowColors.length -1]}`);
+// console.log(`Blue index: ${rainbowColors.indexOf("blue")}`);
 
-// // //Bonus
-// // let twoColors = rainbowColors.slice(1,3);
-// // console.log(twoColors);
+// //Bonus
+// let twoColors = rainbowColors.slice(1,3);
+// console.log(twoColors);
 
-// // const nums = [0, 1, 2, 2, 2, 3, 3, 4, 5];
+// const nums = [0, 1, 2, 2, 2, 3, 3, 4, 5];
 
 // const marxFamily = [
 //     { name: "Groucho", birthYear: 1890 },
@@ -41,60 +41,131 @@
 //     ]
 // };
 
-// The Reading List
-class book {
-    constructor(title, author, isRead){
-        this.title = title,
-        this.author = author,
-        this.isRead = isRead
+// // The Reading List
+// class book {
+//     constructor(title, author, isRead){
+//         this.title = title,
+//         this.author = author,
+//         this.isRead = isRead
+//     }
+// }
+
+// let books = [
+//     new book("Swann's Way","Marcel Proust",false),
+//     new book("The Guermantes Way","Marcel Proust",true),
+//     new book("A Life on Our Planet","David Attenborough",false), 
+//     new book("Sapiens","Yuval Noah Harari",true) 
+// ]
+
+// books.forEach(book => { 
+//     if(book.isRead)
+//         console.log(`You have already read "${book.title}", by ${book.author}.`)
+//     else
+//         console.log(`You still need to read "${book.title}", by ${book.author}.`)
+// })
+
+// // The Recipe Card
+// let recipe = {
+//     title: "Ginger, Apple and Banana Smoothie",
+//     numberOfServes: 2,
+//     ingredients: [
+//         "- 500ml Milk",
+//         "- 2/3 cups of vanilla protein powder",
+//         "- 2 tbs rolled oats",
+//         "- Pinch of cinnamon",
+//         "- 2 cups of baby spinach leaves",
+//         "- 2 frozen bananas",
+//         "- 2 roughly chopped apples",
+//         "- 1/2 avocado",
+//         "- 2 tsp fresh ginger",
+//         "- 6 ice cubes"
+//     ]
+// }
+
+
+// console.log(`${recipe.title}\n\nServes: ${recipe.numberOfServes}\n\nIngredients:\n\n${recipe.ingredients.join('\n')}`)
+// // console.log(`${recipe.title}\n\nServes: ${recipe.numberOfServes}\n\nIngredients:\n\n${recipe.ingredients.forEach(e => {e += `- ${e}`})}`)
+
+// //The Movie Database
+// class movie{
+//     constructor(title,duration,director,stars){
+//         this.title = title,
+//         this.duration = duration,
+//         this.director = director,
+//         this.stars = stars
+//     }
+// }
+
+// let m = new movie("The Life Aquatic",118,"Wes Anderson",["Bill Murray", "Cate Blanchett", "Anjelica Huston", "Jeff Goldblum", "Willem Dafoe"])
+// console.log(`'${m.title}' lasts for ${m.duration} minutes, and was directed by ${m.director}. Stars: ${m.stars.join(",")}.`);
+
+//Working With Users
+class User {
+    constructor(email,password,isAdmin){
+        this.email = email,
+        this.password = password,
+        this.isAdmin = isAdmin
     }
 }
 
-let books = [
-    new book("Swann's Way","Marcel Proust",false),
-    new book("The Guermantes Way","Marcel Proust",true),
-    new book("A Life on Our Planet","David Attenborough",false), 
-    new book("Sapiens","Yuval Noah Harari",true) 
+const users = [
+    new User("groucho@ga.co","chicken",true),
+    new User("harpo@ga.co","elephant",false),
+    new User("gummo@ga.co","pinkFairyArmadillo", true),
+    new User("zeppo@ga.co","dumboOctopus",false)
 ]
 
-books.forEach(book => { 
-    if(book.isRead)
-        console.log(`You have already read "${book.title}", by ${book.author}.`)
+function findUser(email, pwd){
+    return users.findIndex(user => user.email == email && user.password == pwd);
+}
+
+function login(email, pwd){
+    let index = findUser(email,pwd);
+    if(index > -1)
+        console.log(`You are logged in as ${email}`);
     else
-        console.log(`You still need to read "${book.title}", by ${book.author}.`)
-})
-
-// The Recipe Card
-let recipe = {
-    title: "Ginger, Apple and Banana Smoothie",
-    numberOfServes: 2,
-    ingredients: [
-        "- 500ml Milk",
-        "- 2/3 cups of vanilla protein powder",
-        "- 2 tbs rolled oats",
-        "- Pinch of cinnamon",
-        "- 2 cups of baby spinach leaves",
-        "- 2 frozen bananas",
-        "- 2 roughly chopped apples",
-        "- 1/2 avocado",
-        "- 2 tsp fresh ginger",
-        "- 6 ice cubes"
-    ]
+        console.log(`Sorry, something went wrong.`);
 }
 
-
-console.log(`${recipe.title}\n\nServes: ${recipe.numberOfServes}\n\nIngredients:\n\n${recipe.ingredients.join('\n')}`)
-// console.log(`${recipe.title}\n\nServes: ${recipe.numberOfServes}\n\nIngredients:\n\n${recipe.ingredients.forEach(e => {e += `- ${e}`})}`)
-
-//The Movie Database
-class movie{
-    constructor(title,duration,director,stars){
-        this.title = title,
-        this.duration = duration,
-        this.director = director,
-        this.stars = stars
+function createAccount(email, pwd, isAdmin){
+    if(!email || !pwd || !isAdmin){
+        console.log(`Sorry, something went wrong`);
+        return;
     }
+    users.push(new User(email,pwd,isAdmin));
 }
 
-let m = new movie("The Life Aquatic",118,"Wes Anderson",["Bill Murray", "Cate Blanchett", "Anjelica Huston", "Jeff Goldblum", "Willem Dafoe"])
-console.log(`'${m.title}' lasts for ${m.duration} minutes, and was directed by ${m.director}. Stars: ${m.stars.join(",")}.`);
+function deleteAccount(email, pwd){
+    let index = findUser(email,pwd);
+    if(index < -1){
+        console.log(`${email} not exist`);
+        return;
+    }
+    users.splice(index,1);
+}
+
+function updateAccount(email,oldpwd, newprop){
+    let index = findUser(email,oldpwd);
+    for(let prop in newprop)
+        users[index][prop] = newprop[prop];
+}
+
+function search(prop,value){
+    let index = users.findIndex(user => user[prop] == value);
+    console.log(users[index]);
+}
+
+//Transit Application
+const stations = ["Museum", "St. James", "Circular Quay", "Wynyard", "Townhall", "Central", "Redfern", "Macdonaldtown", "Newtown"];
+
+function travelFrom(start,end){
+    let a = stations.findIndex(x => x == start);
+    let b = stations.findIndex(x => x == end);
+    
+    let num = b-a;
+    //To make it positive
+    numStop = num * Math.sign(num);
+    let stops = Math.sign(num) === -1? stations.slice(b,a+1).reverse() : stations.slice(a,b+1);
+    console.log(`To go from '${start}' to '${end}' - you'll need to go through ${numStop} stops`)
+    console.log(stops.join(' - '));
+}
